e0f4b635a15f57631367e16f726a01ebc6ec90e3
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-07-31 17:35:28 +0530

    commit

--- index.js
+++ index.js
@@ -4,34 +4,52 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3010;
+const PORT = 3010; // Change the port to 3010
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
 
 // Middleware
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!');
-});
-// create home route which direct to /country/population
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// Function to convert population to various formats
+function formatPopulation(population) {
+  if (population >= 10000000) {
+    // Convert to crore format
+    return (population / 10000000).toFixed(2) + ' crore';
+  } else if (population >= 50000) {
+    // Convert to lakh format
+    return (population / 100000).toFixed(2) + ' lakh';
+  } else {
+    // Convert to thousands format
+    return (population / 1000).toFixed(2) + ' thousand';
+  }
+}
 
 // Route to render the /country/population page
 app.get('/country/population', (req, res) => {
-    // res.send("country popuoation");
   const data = [];
 
   fs.createReadStream(path.join(__dirname, 'data', 'country_population_2021sep.csv'))
     .pipe(csvParser())
     .on('data', (row) => {
-        console.log("row:" , row);
+      // Convert population to various formats
+      row['Population (September 2021)'] = formatPopulation(parseInt(row['Population (September 2021)']));
       data.push(row);
     })
     .on('end', () => {
       res.render('index', { data });
     });
+});
+
+// Route to render the home page
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Test route
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
